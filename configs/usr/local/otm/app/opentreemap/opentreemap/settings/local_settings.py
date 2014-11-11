89951c24ad27502d70968b0e5ad09bac4edc482a
Michael Maurizi <mmaurizi@azavea.com>
2014-11-11 12:59:04 -0500

    Switch from using sendmail to using the file based email backend
    Fixes #16

--- configs/usr/local/otm/app/opentreemap/opentreemap/settings/local_settings.py
+++ configs/usr/local/otm/app/opentreemap/opentreemap/settings/local_settings.py
@@ -16,3 +16,6 @@ DATABASES = {
 
 BROKER_URL = 'redis://localhost:6379/'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/usr/local/otm/emails'
