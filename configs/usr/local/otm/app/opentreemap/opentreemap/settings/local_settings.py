505e3c54a7fefe726a631505fa7f7447efad96df
Steve Lamb <slamb@azavea.com>
2014-11-21 14:56:44 -0500

    Use redis session backend

--- configs/usr/local/otm/app/opentreemap/opentreemap/settings/local_settings.py
+++ configs/usr/local/otm/app/opentreemap/opentreemap/settings/local_settings.py
@@ -14,8 +14,14 @@ DATABASES = {
     }
 }
 
-BROKER_URL = 'redis://localhost:6379/'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/'
+SESSION_ENGINE = 'redis_sessions.session'
+SESSION_REDIS_HOST = 'localhost'
+SESSION_REDIS_PORT = 6379
+SESSION_REDIS_DB = 1
+SESSION_REDIS_PREFIX = 'session'
+
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = '/usr/local/otm/emails'
