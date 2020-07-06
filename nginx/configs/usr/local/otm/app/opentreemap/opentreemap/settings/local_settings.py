94a06bde132616863cda161c2def28429e1204f7
Tom Z <tzinckgraf@gmail.com>
2020-07-05 21:56:09 -0400

    Add everything for the docker-compose

---
+++ nginx/configs/usr/local/otm/app/opentreemap/opentreemap/settings/local_settings.py
@@ -0,0 +1,21 @@
+EXTRA_UNMANAGED_APPS = ('django_extensions',)
+
+STATIC_ROOT = '/usr/local/otm/static'
+MEDIA_ROOT = '/usr/local/otm/media'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'otm',
+        'USER': 'otm',
+        'PASSWORD': 'otm',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
+
+CELERY_BROKER_URL = 'redis://localhost:6379/'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/usr/local/otm/emails'
